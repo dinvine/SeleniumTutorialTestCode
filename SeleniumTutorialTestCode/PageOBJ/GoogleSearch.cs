a7dfc4e8c3a4d8eaad1f51e049807637631704b4
dinvine <dinvine@qq.com>
2020-01-31 22:00:14 +1300

    fist push

---
+++ SeleniumTutorialTestCode/PageOBJ/GoogleSearch.cs
@@ -0,0 +1,63 @@
+﻿
+using OpenQA.Selenium;
+using NUnit.Framework;
+namespace SeleniumTutorialTestCode.PageOBJ
+{
+    class GoogleSearch
+    {
+        IWebDriver webDriver { get; set; }
+
+        public IWebElement textBoxSearch => webDriver.FindElement(By.Name("q"));
+
+        public IWebElement btnSearch => webDriver.FindElement(By.XPath(@"//*[@id='tsf']/div[2]/div[1]/div[2]/div[2]/div[2]/center/input[1]"));
+
+        public IWebElement btnGoodLuck => webDriver.FindElement(By.Name("btnI"));
+
+        public IWebElement gmailLink => webDriver.FindElement(By.LinkText("Gmail"));
+
+        public IWebElement picLink => webDriver.FindElement(By.LinkText("Images"));
+
+        public IWebElement signInLink => webDriver.FindElement(By.LinkText("Sign in"));
+        public GoogleSearch(ref IWebDriver driver)
+        {
+            webDriver = driver;
+            driver.Url = @"http://www.google.com";
+        }
+
+
+        public void searchTextTest(string searchTxt)
+        {
+            textBoxSearch.Clear();
+            textBoxSearch.SendKeys(searchTxt);
+            textBoxSearch.SendKeys(Keys.Enter);
+            // btnSearch.Click();
+            //    btnSearch.Submit();            
+            System.Threading.Thread.Sleep(3000);
+            Assert.That(webDriver.Title.Contains("Selenium"), "test fail due to title does not contain Selenium");
+
+        }
+
+        public void gmailLinkTest()
+        {
+            gmailLink.Click();
+
+            Assert.That(webDriver.Title.Contains("Gmail"), "test fail due to title does not contain Gmail");
+
+        }
+
+        public void picLinkTest()
+        {
+            picLink.Click();
+            Assert.That(webDriver.Title.Equals("Google Images"), "test fail due to title does not equal to Google Images");
+        }
+
+        public void signInLinkTest()
+        {
+            signInLink.Click();
+
+            Assert.That(webDriver.Title.Contains("Sign in"), "test fail due to title does not contain Sign in");
+        }
+
+
+    }
+}
---
+++ SeleniumTutorialTestCode/SeleniumNunitStudy.cs
@@ -0,0 +1,96 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using SeleniumTutorialTestCode.PageOBJ;
+using System;
+
+namespace SeleniumTutorialTestCode
+{
+    [TestFixture]
+    class SeleniumNunitStudy
+    {
+        public IWebDriver WDriver;
+        [SetUp]
+        public void startBrowser()
+        {
+            WDriver = new FirefoxDriver();
+            WDriver.Url = @"http://www.google.com";
+        }
+
+        
+        [Test]
+        public void ChromeHomePageTest()
+        {
+            GoogleSearch googleSearch = new GoogleSearch(ref WDriver);
+            string title = WDriver.Title;
+
+            System.Threading.Thread.Sleep(3000);
+
+            Console.WriteLine("Page title is {0}", title);
+
+            string pageSource = WDriver.PageSource;
+            Console.WriteLine("PageSource is {0}", pageSource);
+
+            int pageSourceLen = 0;
+            pageSourceLen = pageSource.Length;
+            Console.WriteLine("pageSourceLen is {0}", pageSourceLen);
+
+            //test search
+            googleSearch.searchTextTest("Selenium");
+
+
+            //test Navigate().Back();
+            WDriver.Navigate().Back();
+            System.Threading.Thread.Sleep(2000);
+            Assert.That(WDriver.Title.Equals("Google"), "test fail due to title does not equal to Google ");
+
+
+            //test gmailLink
+            googleSearch.gmailLinkTest();
+            System.Threading.Thread.Sleep(2000);
+            WDriver.Navigate().Back();
+            System.Threading.Thread.Sleep(2000);
+
+
+            //test signInLink
+            googleSearch.signInLinkTest();
+            System.Threading.Thread.Sleep(2000);
+
+            WDriver.Navigate().Back();
+            System.Threading.Thread.Sleep(2000);
+
+            //test picLink
+            googleSearch.picLinkTest();
+            System.Threading.Thread.Sleep(2000);
+            WDriver.Navigate().Back();
+            System.Threading.Thread.Sleep(2000);
+
+            //test Navigate().Forward()
+            WDriver.Navigate().Forward();
+            System.Threading.Thread.Sleep(2000);
+            Assert.That(WDriver.Title.Equals("Google Images"), "test fail due to title does not equal to Google Images");
+
+            //test Navigate().Refresh()
+            WDriver.Navigate().Refresh();
+            System.Threading.Thread.Sleep(2000);
+            Assert.That(WDriver.Title.Equals("Google Images"), "test fail due to title does not equal to Google Images");
+            
+
+
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            System.Threading.Thread.Sleep(3000);
+            WDriver.Close();
+        }
+
+        static public void Main()
+        {
+
+        }
+      
+    }
+}
